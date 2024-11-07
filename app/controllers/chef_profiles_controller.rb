b515b631fd96c1f6fa12402026a0cd93f02a1b8c
raedjah1 <106587632+raedjah1@users.noreply.github.com>
2024-11-07 16:52:16 -0500

    fixed

--- app/controllers/chef_profiles_controller.rb
+++ app/controllers/chef_profiles_controller.rb
@@ -1,30 +1,44 @@
 class ChefProfilesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_chef_profile, only: [:show, :update]
+  before_action :set_chef_profile, only: [:show, :edit, :update]
 
   def index
-    chef_profiles = ChefProfile.all
-    render json: chef_profiles
+    @chef_profiles = ChefProfile.all
+    
+    if params[:search].present?
+      @chef_profiles = @chef_profiles.where("specialty ILIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:specialty].present? && params[:specialty] != 'All Specialties'
+      @chef_profiles = @chef_profiles.where(specialty: params[:specialty])
+    end
   end
 
   def show
-    render json: @chef_profile
+    @reviews = @chef_profile.reviews
+  end
+
+  def new
+    @chef_profile = ChefProfile.new
   end
 
   def create
     @chef_profile = current_user.build_chef_profile(chef_profile_params)
     if @chef_profile.save
-      render json: @chef_profile, status: :created
+      redirect_to @chef_profile, notice: 'Your chef profile was successfully created.'
     else
-      render json: { errors: @chef_profile.errors.full_messages }, status: :unprocessable_entity
+      render :new
     end
+  end
+
+  def edit
   end
 
   def update
     if @chef_profile.update(chef_profile_params)
-      render json: @chef_profile, status: :ok
+      redirect_to @chef_profile, notice: 'Your chef profile was successfully updated.'
     else
-      render json: { errors: @chef_profile.errors.full_messages }, status: :unprocessable_entity
+      render :edit
     end
   end
 
@@ -35,6 +49,12 @@ class ChefProfilesController < ApplicationController
   end
 
   def chef_profile_params
-    params.require(:chef_profile).permit(:bio, :specialty, :experience, :availability)
+    params.require(:chef_profile).permit(
+      :bio, 
+      :specialty, 
+      :experience, 
+      :availability, 
+      :hourly_rate
+    )
   end
 end
